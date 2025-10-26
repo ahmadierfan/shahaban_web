786a637f58d26636a958df54fab36fee96cdd557
Erfan <ahmadierfan.it@gmail.com>
2025-10-26 13:55:18 +0330

    edit meta tag

--- components/web/pricing/Plans.vue
+++ components/web/pricing/Plans.vue
@@ -10,10 +10,10 @@
                         d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
                 </svg>
             </div>
-            <h1 class="text-4xl text-gray-900 mb-4">
+            <h2 class="text-4xl text-gray-900 mb-4">
                 پلن‌های <span
                     class="bg-gradient-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent">شهابان</span>
-            </h1>
+            </h2>
             <p class="text-xl text-gray-600 max-w-2xl mx-auto">
                 راه‌حل نرم‌افزاری جامع برای مدیریت کسب‌وکارهای خدماتی
                 <br>
